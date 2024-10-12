4c07171370b41e63ea66edbc0e8b147eb0f22225
kubrakaradirek <karadirekkubra96@gmail.com>
2024-10-12 12:47:53 +0300

    Add project files.

---
+++ SignalRWork/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWork.Hubs
+{
+    public class ChatHub:Hub
+    {
+        //client ==> sunucuya gitmek için kullanılır
+        public async Task SendMessage(string name,string message)
+        {
+            //server ==> clienta
+            await Clients.All.SendAsync("NewMessage",name,message);
+        }
+
+        public async Task SendLocation(string color,int x,int y)
+        {
+            await Clients.All.SendAsync("LocationReceived",Context.ConnectionId,color,x,y);
+        }
+    }
+}
